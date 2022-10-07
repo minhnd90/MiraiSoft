7f2f8368c3eeaf9dfeefc20b8a536808b143cb61
minhnd90 <258006_evn@etgrp.onmicrosoft.com>
2022-10-07 03:31:40 +0000

    First lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string;
-}>();
+defineProps<{ msg: string }>();
 </script>
 
 <template>
@@ -9,9 +7,7 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> + <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
 </template>
