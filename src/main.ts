810927ef17242369c48ef376ad671e6a3fc37a44
minhnd90 <258006_evn@etgrp.onmicrosoft.com>
2022-10-11 10:25:41 +0000

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-import { createApp } from 'vue';
+import './assets/main.scss';
+
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 import App from './App.vue';
 import router from './router';
 
-import './assets/main.scss';
-
 const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 app.use(createPinia());
+app.use(vuetify);
 app.use(router);
 
 app.mount('#app');
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,26 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+const counter = useCounterStore();
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <VBtn flat @click="counter.increment">Clicked: {{ counter.count }}</VBtn>
+    <p class="text-right">Doubled: {{ counter.doubleCount }}</p>
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
+
+    p {
+      flex: 1;
+    }
   }
 }
 </style>
